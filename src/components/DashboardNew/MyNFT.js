e5d365da6002c30d4b68a2cc717400f1aa2d807f
Martinagracy28 <martinagracy28@gmail.com>
2023-03-27 11:36:54 +0530

    commit

--- src/components/DashboardNew/MyNFT.js
+++ src/components/DashboardNew/MyNFT.js
@@ -650,7 +650,13 @@ const MyNFT = () => {
                                               <br />Wallet address :                                          
                                          &nbsp;
                                         <a  href={"https://explorer.aptoslabs.com/account/" + localStorage.getItem("EAWalletAddress")} target="_blank" rel="noreferer noreferrer">
+                                      {localStorage.getItem("EAWalletAddress")  === null || localStorage.getItem("EAWalletAddress")  === "" || localStorage.getItem("EAWalletAddress")  === " " || localStorage.getItem("wallet") === undefined || localStorage.getItem("EAWalletAddress") === '' ? 
+                                      (<>
+                                      </>):
+                                      (<>
                                         <strong>{localStorage.getItem('EAWalletAddress').slice(0,12)}....{localStorage.getItem('EAWalletAddress').slice(50,58)} </strong>
+
+                                      </>)}
                                         &nbsp;
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="bi ms-2 bi-box-arrow-up-right" viewBox="0 0 16 16">
                                                 <path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"/>
