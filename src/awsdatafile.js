8ebd94dc8d2c7f834b6fecf06b2164ccd5519987
Martinagracy28 <martinagracy28@gmail.com>
2023-03-29 23:34:23 +0530

    commit

--- src/awsdatafile.js
+++ src/awsdatafile.js
@@ -370,24 +370,24 @@ export const getNFTDetailsByTypeSale = async(type,sale) =>{
     let key = "BvXlBA50Iw58XBSBZltS2H5P9IwS76f9hojA6aE5";
       //Get method start
      
-      //   let response2 = await fetch(`/platform/v1/nftdetails/${type}/${sale}`, 
-      // {
-      //     headers: {
-      //         'x-api-key': `${key}`    
-      //       },
-      //   }
-      //   )
-
-      let response2 = await fetch(`https://testapi1.stasisonline.in/platform/v1/nftdetails/${type}/${sale}`,{
-        mode: 'no-cors',
-        method: 'GET',
-        headers:{
-          'Access-Control-Allow-Origin': '*' ,
-          'Content-Type':'application/json',
-          'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE'
+        let response2 = await fetch(`/platform/v1/nftdetails/${type}/${sale}`, 
+      {
+          headers: {
+              'x-api-key': `${key}`    
+            },
         }
-      })
-      console.log("response2",response2.Response);
+        )
+
+      // let response2 = await fetch(`https://testapi1.stasisonline.in/platform/v1/nftdetails/${type}/${sale}`,{
+      //   mode: 'no-cors',
+      //   method: 'GET',
+      //   headers:{
+      //     'Access-Control-Allow-Origin': '*' ,
+      //     'Content-Type':'application/json',
+      //     'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE'
+      //   }
+      // })
+      // console.log("response2",response2.Response);
         const data2 = await response2.json();
       console.log("response2 inside", data2)
       return {data2};
