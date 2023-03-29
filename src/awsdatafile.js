2f4586aef402caaea4eaf68eb6746a4d07203418
Martinagracy28 <martinagracy28@gmail.com>
2023-03-29 13:07:26 +0530

    commit

--- src/awsdatafile.js
+++ src/awsdatafile.js
@@ -47,18 +47,22 @@ export const getuserDetailsbywallet = async(address) =>{
    
     let key = "BvXlBA50Iw58XBSBZltS2H5P9IwS76f9hojA6aE5";
       //Get method start
-      
+      try{
         let response2 = await fetch(`/platform/v1/userinfo/${address}`, 
-      {
-          headers: {
-              'x-api-key': `${key}`    
-            },
-        }
-        )
-      //console.log(response2);
-        const data2 = await response2.json();
-      console.log("Api inside", data2)
-      return {data2};
+        {
+            headers: {
+                'x-api-key': `${key}`    
+              },
+          }
+          )
+        //console.log(response2);
+          const data2 = await response2.json();
+        console.log("Api inside", data2)
+        return {data2};
+      }catch(err){
+        console.log("error in vercel",err)
+      }
+        
 }
 export const getAlluserDetails = async() =>{
    
